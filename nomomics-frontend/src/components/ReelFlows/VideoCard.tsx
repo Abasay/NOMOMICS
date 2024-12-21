da1b29f865e7dc80e5a93a61009b0288e9c3f0ad
Abasay <abdulsalamasheem@gmail.com>
2024-12-21 18:36:44 +0100

    fix: fix buf

--- nomomics-frontend/src/components/ReelFlows/VideoCard.tsx
+++ nomomics-frontend/src/components/ReelFlows/VideoCard.tsx
@@ -1,50 +1,56 @@
 import Image, { StaticImageData } from 'next/image';
 import React from 'react';
 
+import avatarImg from '@/public/images/avatar.png';
+
 interface VideoCardProps {
-  title: string;
-  author: string;
-  views: string;
-  timestamp: string;
-  thumbnail: StaticImageData;
-  avatar: StaticImageData;
+	title: string;
+	author: string;
+	views: string;
+	timestamp: string;
+	thumbnail: StaticImageData;
+	avatar?: null;
 }
 
 const VideoCard: React.FC<VideoCardProps> = ({
-  title,
-  author,
-  views,
-  timestamp,
-  thumbnail,
-  avatar,
+	title,
+	author,
+	views,
+	timestamp,
+	thumbnail,
+	avatar,
 }) => {
-  return (
-    <div className='bg-white rounded-lg shadow-md overflow-hidden'>
-      <Image
-        src={thumbnail}
-        alt='Thumbnail'
-        width={300}
-        height={200}
-        className='w-full h-48 object-cover'
-      />
-      <div className='p-4'>
-        <h2 className='text-md font-semibold text-gray-800'>{title}</h2>
-        <div className='flex items-center space-x-2 mt-2'>
-          <Image
-            src={avatar}
-            alt='Author'
-            width={24}
-            height={24}
-            className='w-6 h-6 rounded-full'
-          />
-          <div className='text-sm text-gray-600'>{author}</div>
-        </div>
-        <div className='text-sm text-gray-500'>
-          {views} Views • {timestamp}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='bg-white rounded-lg shadow-md overflow-hidden'>
+			<Image
+				src={thumbnail}
+				alt='Thumbnail'
+				width={300}
+				height={200}
+				className='w-full h-48 object-cover'
+			/>
+			<div className='p-4'>
+				<h2 className='text-md font-semibold text-gray-800'>
+					{title}
+				</h2>
+				<div className='flex items-center space-x-2 mt-2'>
+					<Image
+						src={avatarImg}
+						alt='Author'
+						width={24}
+						height={24}
+						className='w-6 h-6 rounded-full'
+					/>
+					<div className='text-sm text-gray-600'>
+						{author}
+					</div>
+				</div>
+				<div className='text-sm text-gray-500'>
+					{views} Views • {timestamp}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default VideoCard;
