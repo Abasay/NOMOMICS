a0efaaf7e1d76f60022baf9aaacc10f694250acb
Abasay <abdulsalamasheem@gmail.com>
2024-12-22 15:23:52 +0100

    fix

--- nomomics-frontend/src/app/layout.tsx
+++ nomomics-frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { Metadata } from 'next';
@@ -10,8 +10,8 @@ import { ProfileProvider } from './contexts/Profile';
 import { ComicsProvider } from './contexts/Comics';
 
 const roboto = Inter({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'],
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	subsets: ['latin'],
 });
 
 // export const metadata: Metadata = {
@@ -20,59 +20,66 @@ const roboto = Inter({
 // };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  // const [loading, setLoading] = useState(true);
+	// const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => setLoading(false), 4000); // Adjust duration if necessary
-  //   return () => clearTimeout(timer);
-  // }, []);
+	// useEffect(() => {
+	//   const timer = setTimeout(() => setLoading(false), 4000); // Adjust duration if necessary
+	//   return () => clearTimeout(timer);
+	// }, []);
 
-  return (
-    <html lang='en'>
-      <body>
-        {/* {loading ? (
+	return (
+		<html lang='en'>
+			<body>
+				{/* {loading ? (
           <LoadingScreen />
         ) : ( */}
-        <div className={`${roboto} antialiased`}>
-          {/* Your layout structure */}
-          {/* <div className=' absolute top-0 z-50'> */}
-          <Toaster
-            position='top-center'
-            reverseOrder={false}
-            gutter={8}
-            containerClassName=''
-            containerStyle={{}}
-            toastOptions={{
-              // Define default options
-              className: '',
-              duration: 5000,
-              style: {
-                background: '#363636',
-                color: '#fff',
-                zIndex: 10000,
-              },
+				<Suspense fallback={<LoadingScreen />}>
+					<div
+						className={`${roboto} antialiased`}
+					>
+						{/* Your layout structure */}
+						{/* <div className=' absolute top-0 z-50'> */}
+						<Toaster
+							position='top-center'
+							reverseOrder={false}
+							gutter={8}
+							containerClassName=''
+							containerStyle={{}}
+							toastOptions={{
+								// Define default options
+								className: '',
+								duration: 5000,
+								style: {
+									background: '#363636',
+									color: '#fff',
+									zIndex: 10000,
+								},
 
-              // Default options for specific types
-              success: {
-                duration: 3000,
-                // theme: {
-                //     primary: 'green',
-                //     secondary: 'black',
-                // },
-              },
-            }}
-          />
-          {/* </div> */}
-          <ProfileProvider>
-            <ComicsProvider>{children}</ComicsProvider>
-          </ProfileProvider>
-        </div>
-        {/* )} */}
-      </body>
-    </html>
-  );
+								// Default options for specific types
+								success: {
+									duration: 3000,
+									// theme: {
+									//     primary: 'green',
+									//     secondary: 'black',
+									// },
+								},
+							}}
+						/>
+						{/* </div> */}
+						<ProfileProvider>
+							<ComicsProvider>
+								{children}
+							</ComicsProvider>
+						</ProfileProvider>
+					</div>
+				</Suspense>
+
+				{/* )} */}
+			</body>
+		</html>
+	);
 }
