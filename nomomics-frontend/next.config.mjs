b93c43a5a7c68c3c9224c0eb863d917f964ee300
Abasay <abdulsalamasheem@gmail.com>
2024-12-22 15:31:08 +0100

    fix

--- nomomics-frontend/next.config.mjs
+++ nomomics-frontend/next.config.mjs
@@ -1,20 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactDevOverlay: false,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.pdf$/,
-      use: 'file-loader', // Or another appropriate loader
-    });
+	webpack: (config) => {
+		// Add file-loader for .pdf files
+		config.module.rules.push({
+			test: /\.pdf$/,
+			use: 'file-loader', // Or another appropriate loader
+		});
 
-    return config;
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ['res.cloudinary.com', 'lh3.googleusercontent.com'],
-  },
+		// Fallback for canvas module
+		config.resolve.fallback = {
+			...config.resolve.fallback,
+			canvas: false, // Mock canvas during the build
+		};
+
+		return config;
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		domains: ['res.cloudinary.com', 'lh3.googleusercontent.com'],
+	},
 };
 
 export default nextConfig;
