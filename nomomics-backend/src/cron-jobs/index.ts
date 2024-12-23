e6a13494a1479295e78b71e252e6a1f3d148f1b4
Abasay <abdulsalamasheem@gmail.com>
2024-12-23 10:12:41 +0100

    fixes

--- nomomics-backend/src/cron-jobs/index.ts
+++ nomomics-backend/src/cron-jobs/index.ts
@@ -6,7 +6,7 @@ export default async function startCronJobs() {
 	cron.schedule('*/2 * * * *', async () => {
 		try {
 			const response = await axios.get(
-				'https://nomomics.onrender.com'
+				'https://nomomics-1.onrender.com'
 			);
 			console.log(`STATUS: ${response.status}`);
 			console.log(response.data);
